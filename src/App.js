9450c699592f1fcbbdb081a7eaf3c726358bff43
unknown <jose.victor9936@gmail.com>
2022-09-11 16:11:40 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import jpIMG from "./assets/test.png"
+import "./styles.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title"> Bem Vindo!</span>
+            <span className="login-form-title">
+              <img src={jpIMG} alt="Test"/>
+            </span>
+            <div className="wrap-input">
+              <input className="input" type={"email"}/>
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+            <div className="wrap-input">
+              <input className="input" type={"password"}/>
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+            <div className="text-center">
+              <span className="text'">Não possui conta?</span>
+
+              <a className="text2" href="#">Criar conta.</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
